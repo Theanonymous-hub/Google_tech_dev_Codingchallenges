a34f17184a86301dd975c97a24be02ed56afb6f0
Prasana.R <76907455+Theanonymous-hub@users.noreply.github.com>
2023-08-15 22:11:07 +0530

    Delete Day2 directory

--- Day2/if.py
+++
@@ -1,8 +0,0 @@
-'''Write a program which reads an integer from input using pancakes = int(input()).
-If pancakes is more than 3, print out Yum! and if pancakes is not more than 3, print out Still hungry! instead.'''
-
-pancakes = int(input())
-if pancakes>3:
-   print("Yum!")
-else:
-   print("Still hungry!")
